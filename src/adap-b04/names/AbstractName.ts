d876406c375f892373f72664ccb9ac6e333bfec0
Toast34 <tobias.merz34@gmail.com>
2025-11-30 21:56:53 +0100

    Finished b04

--- src/adap-b04/names/AbstractName.ts
+++ src/adap-b04/names/AbstractName.ts
@@ -1,4 +1,7 @@
 import { DEFAULT_DELIMITER, ESCAPE_CHARACTER } from "../common/Printable";
+import { IllegalArgumentException } from "../common/IllegalArgumentException";
+import { InvalidStateException } from "../common/InvalidStateException";
+import { MethodFailedException } from "../common/MethodFailedException";
 import { Name } from "./Name";
 
 export abstract class AbstractName implements Name {
@@ -6,15 +9,23 @@ export abstract class AbstractName implements Name {
     protected delimiter: string = DEFAULT_DELIMITER;
 
     constructor(delimiter: string = DEFAULT_DELIMITER) {
-        throw new Error("needs implementation or deletion");
+        if (delimiter) this.delimiter = delimiter;
     }
 
     public clone(): Name {
-        throw new Error("needs implementation or deletion");
+        var retVal : Name = Object.assign({},this);
+        return retVal;
     }
 
     public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation or deletion");
+            let retVal = "";
+        for (let i = 0; i < this.getNoComponents(); i++) {
+            var component = this.getComponent(i);
+            retVal += component;
+            if (i == this.getNoComponents()-1) return retVal;    
+            retVal += delimiter;
+        }
+        return retVal;
     }
 
     public toString(): string {
@@ -22,23 +33,47 @@ export abstract class AbstractName implements Name {
     }
 
     public asDataString(): string {
-        throw new Error("needs implementation or deletion");
+            let retVal = this.asString();
+            let done = false;
+            let i = 0;
+            while (!done) {
+                done = true;
+                for (i; i < retVal.length; i++) {
+                    const char = retVal[i];
+                    if(char == ESCAPE_CHARACTER) {
+                        done = false;
+                    }
+                    if (!done) break;
+                }
+                if (!done) {
+                    retVal = retVal.slice(0,i) + ESCAPE_CHARACTER + ESCAPE_CHARACTER + retVal.slice(i+1);
+                }
+                i+=2;
+            }
+            return retVal;
     }
 
     public isEqual(other: Name): boolean {
-        throw new Error("needs implementation or deletion");
+        return this.asString() === other.asString();
     }
 
     public getHashCode(): number {
-        throw new Error("needs implementation or deletion");
+        let hashCode: number = 0;
+        const s: string = this.asDataString();
+        for (let i: number = 0; i < s.length; i++) {
+            let c: number = s.charCodeAt(i);
+            hashCode = (hashCode << 5) - hashCode + c;
+            hashCode |= 0;
+        }
+        return hashCode;
     }
 
     public isEmpty(): boolean {
-        throw new Error("needs implementation or deletion");
+        return this.getNoComponents() == 0;
     }
 
     public getDelimiterCharacter(): string {
-        throw new Error("needs implementation or deletion");
+        return this.delimiter;
     }
 
     abstract getNoComponents(): number;
@@ -51,7 +86,9 @@ export abstract class AbstractName implements Name {
     abstract remove(i: number): void;
 
     public concat(other: Name): void {
-        throw new Error("needs implementation or deletion");
+        for (let i = 0; i < other.getNoComponents(); i++) {
+            this.append(other.getComponent(i));
+        }
     }
 
 }
--- src/adap-b04/names/StringName.ts
+++ src/adap-b04/names/StringName.ts
@@ -1,6 +1,9 @@
 import { DEFAULT_DELIMITER, ESCAPE_CHARACTER } from "../common/Printable";
 import { Name } from "./Name";
 import { AbstractName } from "./AbstractName";
+import { IllegalArgumentException } from "../common/IllegalArgumentException";
+import { InvalidStateException } from "../common/InvalidStateException";
+import { MethodFailedException } from "../common/MethodFailedException";
 
 export class StringName extends AbstractName {
 
@@ -8,64 +11,58 @@ export class StringName extends AbstractName {
     protected noComponents: number = 0;
 
     constructor(source: string, delimiter?: string) {
-        super();
-        throw new Error("needs implementation or deletion");
-    }
-
-    public clone(): Name {
-        throw new Error("needs implementation or deletion");
+        super(delimiter);
+        this.name = source;
+        this.noComponents = this.name.split(this.delimiter).length;
     }
 
     public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation or deletion");
-    }
-
-    public asDataString(): string {
-        throw new Error("needs implementation or deletion");
-    }
-
-    public isEqual(other: Name): boolean {
-        throw new Error("needs implementation or deletion");
-    }
-
-    public getHashCode(): number {
-        throw new Error("needs implementation or deletion");
-    }
-
-    public isEmpty(): boolean {
-        throw new Error("needs implementation or deletion");
-    }
-
-    public getDelimiterCharacter(): string {
-        throw new Error("needs implementation or deletion");
+        return this.name;
     }
 
     public getNoComponents(): number {
-        throw new Error("needs implementation or deletion");
+        return this.noComponents;
     }
 
-    public getComponent(i: number): string {
-        throw new Error("needs implementation or deletion");
+    public getComponent(x: number): string {
+        IllegalArgumentException.assert(x >= 0 && x < this.getNoComponents());
+        return this.name.split(this.delimiter)[x];
     }
 
-    public setComponent(i: number, c: string) {
-        throw new Error("needs implementation or deletion");
+    public setComponent(n: number, c: string): void {
+        IllegalArgumentException.assert(n >= 0 && n < this.getNoComponents());
+        let newString = this.name.split(this.delimiter);
+        newString[n] = c;
+        this.name = newString.join(this.delimiter);
     }
 
-    public insert(i: number, c: string) {
-        throw new Error("needs implementation or deletion");
+    public insert(n: number, c: string): void {
+        IllegalArgumentException.assert(n >= 0 && n < this.getNoComponents());
+        let newString = this.name.split(this.delimiter);
+        newString.splice(n,0,c)
+        this.name = newString.join(this.delimiter);
+        this.noComponents++;
     }
 
-    public append(c: string) {
-        throw new Error("needs implementation or deletion");
+    public append(c: string): void {
+        let newString = this.name.split(this.delimiter);
+        newString[newString.length] = c;
+        this.name = newString.join(this.delimiter);
+        this.noComponents++;
     }
 
-    public remove(i: number) {
-        throw new Error("needs implementation or deletion");
+    public remove(n: number): void {
+        IllegalArgumentException.assert(n >= 0 && n < this.getNoComponents());
+        if (this.noComponents == 0) return;
+        let newString = this.name.split(this.delimiter);
+        newString.splice(n,1);
+        this.name = newString.join(this.delimiter);
+        this.noComponents--;
     }
 
     public concat(other: Name): void {
-        throw new Error("needs implementation or deletion");
+        this.name += other.asString();
+        this.noComponents += other.getNoComponents();
     }
 
 }
